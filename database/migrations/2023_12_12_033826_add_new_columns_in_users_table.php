6e6cff02512fd7dc702e0364b86d7a04a85faa07
Gauravkandel <gauravkandell990@gmail.com>
2023-12-12 12:42:04 +0545

    push

--- database/migrations/2023_12_12_033826_add_new_columns_in_users_table.php
+++ database/migrations/2023_12_12_033826_add_new_columns_in_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->boolean('ActiveStatus')->default(false);
-            $table->timestamp('ActiveTime')->nullable();
+            $table->timestamp('ActiveTime')->default(now());
         });
     }
 
