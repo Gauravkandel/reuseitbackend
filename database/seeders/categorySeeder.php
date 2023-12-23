b00eeab260d498e0cb58a4e4c019b5853822290a
Gauravkandel <gauravkandell990@gmail.com>
2023-12-23 17:33:38 +0545

    taan

--- database/seeders/categorySeeder.php
+++ database/seeders/categorySeeder.php
@@ -14,20 +14,62 @@ class categorySeeder extends Seeder
     public function run(): void
     {
         $cat_data = [
-            ['category_name' => 'Electronics'],
-            ['category_name' => 'Home Appliances'],
-            ['category_name' => 'Furniture'],
-            ['category_name' => 'Clothing and Accessories'],
-            ['category_name' => 'Sports and Fitness'],
-            ['category_name' => 'Books and Media'],
-            ['category_name' => 'Antiques and Collectibles'],
-            ['category_name' => 'Cars'],
-            ['category_name' => 'Motorcycles'],
-            ['category_name' => 'Scooters'],
-            ['category_name' => 'Bicycles'],
-            ['category_name' => 'Toys and Games'],
-            ['category_name' => 'Musical Instruments'],
-            ['category_name' => 'Others'],
+            [
+                'category_name' => 'Electronics',
+                'function_name' => 'electronics'
+            ],
+            [
+                'category_name' => 'Home Appliances',
+                'function_name' => 'homeAppliances'
+            ],
+            [
+                'category_name' => 'Furniture',
+                'function_name' => 'furnitures'
+            ],
+            [
+                'category_name' => 'Clothing and Accessories',
+                'function_name' => 'clothing'
+            ],
+            [
+                'category_name' => 'Sports and Fitness',
+                'function_name' => 'sports'
+            ],
+            [
+                'category_name' => 'Books and Media',
+                'function_name' => 'books'
+            ],
+            [
+                'category_name' => 'Antiques and Collectibles',
+                'function_name' => 'antiques'
+            ],
+            [
+                'category_name' => 'Cars',
+                'function_name' => 'cars'
+            ],
+            [
+                'category_name' => 'Motorcycles',
+                'function_name' => 'motorcycle'
+            ],
+            [
+                'category_name' => 'Scooters',
+                'function_name' => 'scooter'
+            ],
+            [
+                'category_name' => 'Bicycles',
+                'function_name' => 'bicycle'
+            ],
+            [
+                'category_name' => 'Toys and Games',
+                'function_name' => 'toys'
+            ],
+            [
+                'category_name' => 'Musical Instruments',
+                'function_name' => 'music'
+            ],
+            [
+                'category_name' => 'Others',
+                'function_name' => 'others'
+            ],
         ];
         foreach ($cat_data as $item) {
             category::create($item);
